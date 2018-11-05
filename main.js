f67915f04339fec9e914c8f807389e38b60c8379
Fernando M Tenório <fernando.tenorio@tecnospeed.com.br>
2018-11-05 12:47:00 -0200

    add comments in code

--- main.js
+++ main.js
@@ -28,14 +28,21 @@ app.on('activate', () => {
   }
 })
 
+// Receive async message from renderer
+// See file renderer.js on line 3
 ipcMain.on('ping-good', event => {
+  // It's so good because below have a delay 5s to execute, and this don't lock rendereder :(
   setTimeout(() => {
     console.log('GOOD finshed!')
+    // Send reply to a renderer
     event.sender.send('ping-good-reply', 'pong')
   }, 5000)
 })
 
+// Receive sync message from renderer
+// See file renderer.js on line 18
 ipcMain.on('ping-bad', event => {
+  // It's so bad because below have a delay 5s to execute, meanwhile the renderer stay locked :(
   setTimeout(() => {
     console.log('BAD finshed!')
     event.returnValue = 'pong'
--- renderer.js
+++ renderer.js
@@ -1,14 +1,22 @@
 const { ipcRenderer } = require('electron')
 
+// GOOD
 document.getElementById('ping-good').onclick = () => {
+  // Send a IPC async message to electron
+  // See main.js on line 31
   ipcRenderer.send('ping-good', 'ping')
   document.getElementById('ping-good-response').innerText = 'Waiting..'
 }
 
+// Receive reply from elecron
+// See file main.js on line 37
 ipcRenderer.on('ping-good-reply', (event, response) => {
   document.getElementById('ping-good-response').innerText = response
 })
 
+// BAD
 document.getElementById('ping-bad').onclick = () => {
+  // Send a IPC sync message to electron
+  // See main.js on line 42
   document.getElementById('ping-bad-response').innerText = ipcRenderer.sendSync('ping-bad', 'ping')
 }
